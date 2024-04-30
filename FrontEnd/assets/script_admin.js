bb27a41dbdb8a8bdb79362c47157d5753bd4d719
Lopimaterkus <julien.baraer29@gmail.com>
2024-04-30 21:28:56 +0200

    Ajout de la fonction fermeture de la modale

--- FrontEnd/assets/script_admin.js
+++ FrontEnd/assets/script_admin.js
@@ -17,7 +17,7 @@ async function importImages() {
   }
   
   importImages();
-  
+ 
   /* Modale */
   
   let modal = null;
@@ -40,3 +40,55 @@ async function importImages() {
       .addEventListener("click", stopPropagation);
     modalLoad();
   };
+  
+  /* Fermeture Modale */
+  const closeModal = function (e) {
+    if (modal === null) return; 
+    e.preventDefault();
+    const targetWrapper = document.querySelector(".img-load-container");
+    targetWrapper.innerHTML = "";
+    modal.style.display = "none";
+    const loadContainer = document.querySelector(".img-load-container");
+    const deleteGallery = document.querySelector(".delete-gallery");
+    const previousModal = document.querySelector(".back");
+    const workForm = document.querySelector("#my-form");
+    const titleModal = document.querySelector("#titlemodal");
+    const contentBottom = document.querySelector(".bottom-content");
+    const uploadedImg = document.querySelector("#uploadedimage");
+    uploadedImg.innerHTML = "";
+    uploadedImg.style.width = "0px";
+    uploadedImg.style.height = "0px";
+    contentBottom.style.display = "flex";
+    previousModal.style.display = "none";
+    loadContainer.style.display = "flex";
+    titleModal.innerHTML = "Galerie photo";
+    deleteGallery.style.display = "block";
+    workForm.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+    modal.removeAttribute("aria-modal");
+    modal.removeAttribute("click", closeModal);
+    modal
+      .querySelector(".close")
+      .removeEventListener("click", closeModal);
+    modal
+      .querySelector(".js-modal-stop")
+      .removeEventListener("click", closeModal);
+    modal = null;
+  };
+  
+  const stopPropagation = function (e) {
+    e.stopPropagation();
+  };
+  
+  document.querySelectorAll(".js-modal").forEach((a) => {
+    a.addEventListener("click", openModal);
+  });
+  
+  window.addEventListener("keydown", function (e) {
+    /*Ajout de bind pour fermer la Modale*/ if (
+      e.key === "Escape" ||
+      e.key === "Esc"
+    ) {
+      closeModal(e);
+    }
+  });
